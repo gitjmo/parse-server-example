e2ef6ad7c8c44b0d261514cc747967a2e3abf751
gitjmo <moczygemba.jason@gmail.com>
2016-09-13 16:05:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ app.get('/', function(req, res) {
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
+//app.get('/test', function(req, res) {
+//  res.sendFile(path.join(__dirname, '/public/test.html'));
+//});
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
